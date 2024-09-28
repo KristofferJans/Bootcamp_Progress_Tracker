92ec6514e9b1208e06f854d79a452d1430461d1b
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-29 00:14:03 +0200

    add MongoDBAdapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/db/mongodb";
 
 export const authOptions = {
