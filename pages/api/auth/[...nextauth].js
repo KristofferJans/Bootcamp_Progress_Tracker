e7f3cc9dcec0db1d58fc5b07f0bdb89f1b7fd800
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-29 00:06:57 +0200

    add MongoDBAdapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import client from "@/db/mongodb";
 
 export const authOptions = {
   // Configure one or more authentication providers
+  adapter: MongoDBAdapter(client),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
