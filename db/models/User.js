e04b4d4f7da0f2fd839f8a778aa688a105c99879
Kristoffer Jans <jans.kiki@gmail.com>
2024-10-10 14:22:11 +0200

    write userSchema

---
+++ db/models/User.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const progressSchema = new mongoose.Schema({
+  challenge_id: { type: Schema.Types.ObjectId, required: false },
+  points: { type: Number, min: 1, max: 4, required: false },
+});
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  emailVerified: { type: String, required: true }, //??
+  progress: { type: [progressSchema], default: [] },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
