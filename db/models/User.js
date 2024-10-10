d4ec97f2800fbe5692a2f74f5a07b7d8939bcc8a
Kristoffer Jans <jans.kiki@gmail.com>
2024-10-10 14:37:33 +0200

    modify user schema

--- db/models/User.js
+++ db/models/User.js
@@ -7,23 +7,26 @@ const progressSchema = new mongoose.Schema({
   points: { type: Number, min: 1, max: 4, required: false },
 });
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  emailVerified: { type: String, required: true }, //??
-  finishedChallenges: [{ type: Schema.Types.ObjectId, ref: "Challenge" }],
-  progress: [
-    {
-      challenge_id: {
-        type: Schema.Types.ObjectId,
-        required: false,
-        ref: "Challenge",
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    emailVerified: { type: String, required: true }, //??
+    finishedChallenges: [{ type: Schema.Types.ObjectId, ref: "Challenge" }],
+    progress: [
+      {
+        challenge_id: {
+          type: Schema.Types.ObjectId,
+          required: false,
+          ref: "Challenge",
+        },
+        progressLevel: { type: Number, min: 1, max: 4, required: false },
       },
-      progressLevel: { type: Number, min: 1, max: 4, required: false },
-    },
-  ],
-});
+    ],
+  },
+  { collection: "users" } // Explicitly refer to the correct collection
+);
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
