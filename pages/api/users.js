ee46986731d349af544d1c77fea8e9cd18f8dc52
Kristoffer Jans <jans.kiki@gmail.com>
2024-10-10 14:50:31 +0200

    write user api route

--- pages/api/users.js
+++ pages/api/users.js
@@ -2,12 +2,21 @@ import dbConnect from "@/db/connect";
 import User from "@/db/models/User";
 
 export default async function handler(request, response) {
-    await dbConnect();
+  await dbConnect();
+  const { id } = request.query;
 
-    if (request.method === "GET") {
-        try {
-            const {id} = request.query;
-            
-        }
+  if (request.method === "GET") {
+    try {
+      const userData = await User.findById(id).populate(
+        "finishedChallenges progress.challengeId"
+      );
+      console.log("Fetched User-Data", userData);
+      return response.status(200).json(userData);
+    } catch (error) {
+      console.error("Error fetching user-data:", error);
+      return response.status(500).json({ message: "Internal server error" });
     }
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
